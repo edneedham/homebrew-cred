4bd33e8331a2fc740b032d29fe488388c01db764
Edward Needham <edwardneedham123@gmail.com>
2025-12-10 12:52:09 -0300

    Add formula

---
+++ Formula/cred.rb
@@ -0,0 +1,17 @@
+class Cred < Formula
+    desc "Local-first credential manager"
+    homepage "https://github.com/edneedham/cred"
+    url "https://github.com/edneedham/cred/archive/refs/tags/v0.1.3.tar.gz"
+    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    license "MIT OR Apache-2.0"
+  
+    depends_on "rust" => :build
+  
+    def install
+      system "cargo", "install", *std_cargo_args
+    end
+  
+    test do
+      assert_match version.to_s, shell_output("#{bin}/cred --version")
+    end
+  end
